6429caa51e4572591989bbc97de4bd88439a0963
Daniel <danigogar@gmail.com>
2026-04-09 22:57:38 +0200

    implementar sistema de préstamos (máximo 3, control de disponibilidad)

---
+++ ejercicios/T9/src/routes/loans.routes.js
@@ -0,0 +1,10 @@
+﻿import { Router } from 'express';
+import { getMyLoans, getAllLoans, createLoan, returnBook } from '../controllers/loans.controller.js';
+import { authenticate, authorize } from '../middleware/auth.middleware.js';
+
+const router = Router();
+router.get('/', authenticate, getMyLoans);
+router.get('/all', authenticate, authorize('LIBRARIAN', 'ADMIN'), getAllLoans);
+router.post('/:bookId', authenticate, createLoan);
+router.put('/:id/return', authenticate, returnBook);
+export default router;
