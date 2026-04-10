f1c45f938bedad6e2d4e06259f9007cc94f4f05c
Daniel <danigogar@gmail.com>
2026-04-10 21:40:03 +0200

    autenticación (jwt)

---
+++ ejercicios/T10/src/utils/jwt.js
@@ -0,0 +1,23 @@
+﻿import jwt from 'jsonwebtoken';
+
+const JWT_SECRET = process.env.JWT_SECRET;
+
+if (!JWT_SECRET) {
+  throw new Error('JWT_SECRET no está definido en las variables de entorno');
+}
+
+export function generateToken(user) {
+  return jwt.sign(
+    { id: user._id, email: user.email, name: user.name },
+    JWT_SECRET,
+    { expiresIn: '7d' }
+  );
+}
+
+export function verifyToken(token) {
+  try {
+    return jwt.verify(token, JWT_SECRET);
+  } catch (error) {
+    return null;
+  }
+}
