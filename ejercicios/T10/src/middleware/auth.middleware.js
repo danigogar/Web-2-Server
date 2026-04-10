fe16335d9862c658f3c294510e51718550aae99a
Daniel <danigogar@gmail.com>
2026-04-10 21:40:32 +0200

    middleware

---
+++ ejercicios/T10/src/middleware/auth.middleware.js
@@ -0,0 +1,19 @@
+﻿import { verifyToken } from '../utils/jwt.js';
+
+export function authenticate(req, res, next) {
+  const authHeader = req.headers.authorization;
+
+  if (!authHeader || !authHeader.startsWith('Bearer ')) {
+    return res.status(401).json({ error: true, message: 'Token no proporcionado' });
+  }
+
+  const token = authHeader.split(' ')[1];
+  const decoded = verifyToken(token);
+
+  if (!decoded) {
+    return res.status(401).json({ error: true, message: 'Token inválido o expirado' });
+  }
+
+  req.user = decoded;
+  next();
+}
