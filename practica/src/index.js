30493ecdcd943375bd504c2c52e3eee8b752bd22
Daniel <danigogar@gmail.com>
2026-04-13 19:53:36 +0200

    configurar Express con middleware global

---
+++ practica/src/index.js
@@ -0,0 +1,21 @@
+﻿import app from './app.js';
+import { config } from './config/index.js';
+
+const PORT = config.port;
+
+const server = app.listen(PORT, () => {
+  console.log(`🏢 BildyApp API corriendo en http://localhost:${PORT}`);
+  console.log(`📝 Entorno: ${config.nodeEnv}`);
+});
+
+process.on('SIGINT', async () => {
+  console.log('\n🔌 Cerrando servidor...');
+  server.close();
+  process.exit(0);
+});
+
+process.on('SIGTERM', () => {
+  console.log('\n🔌 Cerrando servidor...');
+  server.close();
+  process.exit(0);
+});
