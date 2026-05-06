1758851cf38b98c5a33bc353d7d5763d33be477a
Daniel <danigogar@gmail.com>
2026-05-06 10:18:07 +0200

    actualización de envs, tests, docker y demás (pruebas por hacer con docker y jest)

---
+++ practica/jest.config.js
@@ -0,0 +1,14 @@
+﻿export default {
+  testEnvironment: 'node',
+  transform: {},
+  extensionsToTreatAsEsm: ['.js'],
+  setupFilesAfterEnv: ['./tests/setup.js'],
+  coverageThreshold: {
+    global: {
+      branches: 70,
+      functions: 70,
+      lines: 70,
+      statements: 70
+    }
+  }
+};
